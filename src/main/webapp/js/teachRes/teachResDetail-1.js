005a3f53eedb03e213f9036df78812383c596f8a
Charming-Victim <842721197@qq.com>
2018-12-17 21:06:18 +0800

    稍微修改一下返回的布局

--- src/main/webapp/js/teachRes/teachResDetail-1.js
+++ src/main/webapp/js/teachRes/teachResDetail-1.js
@@ -1,5 +1,5 @@
 function Preview() {
-    document.getElementById("resource").innerHTML="<a href=\"teachResDetail-1.html\"><img src=\"../images/teachResource/返回.png\"></a>\n" +
+    document.getElementById("resource").innerHTML="<a href=\"teachResDetail-1.html\"><img src=\"../images/teachResource/返回.png\">返回</a>\n" +"<br>"+
         "<b style=\"font-size: 25px;\">2017-18-1--面向对象分析与设计(UML)--期中考试试卷A-答案.pdf</b>\n" +
         "<div class=\"embed\\\">\n" +
         "<embed src=\"../resource/2017-18-1--面向对象分析与设计(UML)--期中考试试卷A-答案.pdf\" width=\"800\" height=\"800\">\n" +
