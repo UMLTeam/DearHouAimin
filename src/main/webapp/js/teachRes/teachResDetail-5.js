5f05fa2323f29447d2a2dcbf86a99c9bd2c39d57
Gjxl <1490413372@qq.com>
2018-12-18 15:00:28 +0800

    添加教学资源-实验任务预览

---
+++ src/main/webapp/js/teachRes/teachResDetail-5.js
@@ -0,0 +1,22 @@
+
+function Preview(e) {
+    var clicked=e.target;
+    var target=clicked.parentElement.nextElementSibling;
+    var fileName=target.getAttribute("download");
+    var patternFileExtension = /\.([0-9a-z]+)(?:[\?#]|$)/i;
+    var extension=(fileName).match(patternFileExtension);
+    //大部分的src:../resoure/XXX.pdf
+    //ppt使用这个src格式
+    //'https://view.officeapps.live.com/op/view.aspx?src=http://storage.xuetangx.com/public_assets/xuetangx/PDF/1.xls'
+    if(extension[1]=="ppt"||extension[1]=="pptx"){
+        var src="https://view.officeapps.live.com/op/view.aspx?src=http://www.niracler.com/resource/"+fileName;
+    }
+    else{
+        var src="../resource/"+fileName;
+    }
+    document.getElementById("resource").innerHTML="<a href=\"teachResDetail-1.html\"><img src=\"../images/teachResource/返回.png\">返回</a>\n" +"<br>"+
+        "<b style=\"font-size: 25px;\">"+fileName+"</b>\n" +
+        "<div class=\"embed\\\">\n" +
+        "<embed src=\""+src+"\" width=\"800\" height=\"800\">\n" +
+        "</div>";
+}
