e3b5bd22e81d861c8d7c6e9a184e9583f1122e40
niracler <niracler@niracler-pc>
2018-12-25 14:44:20 +0800

    修正数据库名称, 日志文件位置, 添加sql文件

---
+++ src/test/table.sql
@@ -0,0 +1,13 @@
+-- auto-generated definition
+create table resource
+(
+  id      int(11) unsigned auto_increment comment 'ID'
+    primary key,
+  resName varchar(255)                        not null comment '名称',
+  resTime timestamp default CURRENT_TIMESTAMP not null comment '上传时间',
+  resPath varchar(255)                        not null comment '上传路径',
+  resType varchar(255)                        not null comment '资源类型',
+  resTag  varchar(255)                        not null comment '标签',
+  isCheck varchar(255)                        not null comment '发布状态'
+)
+  charset = utf8;
