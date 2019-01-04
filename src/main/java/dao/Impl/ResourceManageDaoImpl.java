dac544c07ba4a4cc7ca50c55428306b0e1bae841
yinwei <844116221@qq.com>
2019-01-04 12:22:58 +0800

    update and merge vuzcrc

--- src/main/java/dao/Impl/ResourceManageDaoImpl.java
+++ src/main/java/dao/Impl/ResourceManageDaoImpl.java
@@ -6,7 +6,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import sun.util.calendar.BaseCalendar;
 
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,244 +15,244 @@ import java.util.List;
  * @Date: 2018/12/24 23:50
  */
 public class ResourceManageDaoImpl implements ResourceManageDao {
-    Logger logger = LogManager.getLogger(ResourceManageDaoImpl.class);
-    private Connection connection=null;
-    private PreparedStatement preparedStatement=null;
-    private ResultSet resultSet=null;
-    
-    private static DataBaseConnectionImpl dataBaseConnection;
+	Logger logger = LogManager.getLogger(ResourceManageDaoImpl.class);
+	private Connection connection = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
 
-    public ResourceManageDaoImpl(){
-        dataBaseConnection = new DataBaseConnectionImpl();
-        connection = dataBaseConnection.getConnection();
-    }
+	private static DataBaseConnectionImpl dataBaseConnection;
 
-    @Override
-    protected void finalize( )
-    {
-        dataBaseConnection.free(connection, preparedStatement, resultSet);
-    }
+	public ResourceManageDaoImpl() {
+		dataBaseConnection = new DataBaseConnectionImpl();
+		connection = dataBaseConnection.getConnection();
+	}
 
-    @Override
-    public List<Resource> selectAll(){
-        List<Resource> resources = new ArrayList<Resource>();
-        Resource resource;
-        String sql = "select id, resName, resTime, resPath, resType, isCheck from resource";
-        
-        try{
-            preparedStatement = connection.prepareStatement(sql);
-            resultSet =  preparedStatement.executeQuery(sql);
-            while(resultSet.next()){
-                resource = new Resource();
-                resource.setId(resultSet.getInt("id"));
-                resource.setResName(resultSet.getString("resName"));
-                resource.setResTime(resultSet.getTimestamp("resTime"));
-                resource.setResPath(resultSet.getString("resPath"));
-                resource.setResType(resultSet.getString("resType"));
-                resource.setIsCheck(resultSet.getString("isCheck"));
-                resources.add(resource);
-            }
-            return resources;
-        }catch(Exception e){
-            logger.error(e.toString());
-            return null;
-        }finally {
-            dataBaseConnection.free(null,preparedStatement,resultSet);
-        }
-    }
-    
-    @Override
-    public List<Resource> selectByType(String type) {
-        List<Resource> resources = new ArrayList<Resource>();
-        Resource resource;
-        String sql = "select * from resource where resType like ?";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, type);
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                resource = new Resource();
-                resource.setId(resultSet.getInt("id"));
-                resource.setResName(resultSet.getString("resName"));
-                resource.setResTime(resultSet.getTimestamp("resTime"));
-                resource.setResPath(resultSet.getString("resPath"));
-                resource.setResType(resultSet.getString("resType"));
-                resource.setIsCheck(resultSet.getString("isCheck"));
-                resources.add(resource);
-            }
-            return resources;
-        } catch (Exception e) {
-            logger.error(e.toString());
-            return null;
-        } finally {
-            dataBaseConnection.free(null, preparedStatement, resultSet);
-        }
-    }
-    
-    @Override
-    public List<Resource> selectByDate(Timestamp date1, Timestamp date2,String resType) {
-        List<Resource> resources = new ArrayList<Resource>();
-        Resource resource;
-        String sql = "select id,resName,resTime,resPath,resType,isCheck from resource where resType='"+resType+"' and resTime between date1 and date2";
-        try{
-            preparedStatement = connection.prepareStatement(sql);
-            resultSet = preparedStatement.executeQuery(sql);
-            while(resultSet.next()){
-                resource = new Resource();
-                resource.setId(resultSet.getInt("id"));
-                resource.setResName(resultSet.getString("resName"));
-                resource.setResTime(resultSet.getTimestamp("resTime"));
-                resource.setResPath(resultSet.getString("resPath"));
-                resource.setResType(resultSet.getString("resType"));
-                resource.setIsCheck(resultSet.getString("isCheck"));
-                resources.add(resource);
-            }
-            return resources;
-        }catch(Exception e){
-            logger.error(e.toString());
-            return null;
-        }finally {
-            dataBaseConnection.free(null, preparedStatement, resultSet);
-        }
-    }
-    
-    @Override
-    public List<Resource> selectByFuzzyName(String name,String resType) {
-        List<Resource> resources = new ArrayList<>();
-        Resource resource;
-        String sql = "select * from resource where resType='"+resType+"' and resName like '%"+name+"%'";
-        try{
-            preparedStatement = connection.prepareStatement(sql);
-            resultSet = preparedStatement.executeQuery();
-            while(resultSet.next()){
-                resource = new Resource();
-                resource.setId(resultSet.getInt("id"));
-                resource.setResName(resultSet.getString("resName"));
-                resource.setResTime(resultSet.getTimestamp("resTime"));
-                resource.setResPath(resultSet.getString("resPath"));
-                resource.setResType(resultSet.getString("resType"));
-                resource.setIsCheck(resultSet.getString("isCheck"));
-                resources.add(resource);
-            }
-            return resources;
-        }catch(Exception e){
-            logger.error(e.toString());
-            return null;
-        }finally {
-            dataBaseConnection.free(null, preparedStatement, resultSet);
-        }
-    }
-    
-    @Override
-    public int update(Resource resource) {
-        String sql = "update resource set resName = ?, resPath = ?, resType = ?, isCheck = ? where id = ?";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, resource.getResName());
-            preparedStatement.setString(2, resource.getResPath());
-            preparedStatement.setString(3, resource.getResType());
-            preparedStatement.setString(4, resource.getIsCheck());
-            preparedStatement.setInt(5, resource.getId());
-            return preparedStatement.executeUpdate();
-        } catch (Exception e) {
-            logger.error(e.toString());
-            return 0;
-        } finally {
-            dataBaseConnection.free(null, preparedStatement, null);
-        }
-    }
-    
-    @Override
-    public int updateCheck(Resource resource) {
-        String sql = "update resource set isCheck = ? where id = ?";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, resource.getIsCheck());
-            preparedStatement.setInt(2, resource.getId());
-            return preparedStatement.executeUpdate();
-        } catch (Exception e) {
-            logger.error(e.toString());
-            return 0;
-        } finally {
-            dataBaseConnection.free(null, preparedStatement, null);
-        }
-    }
-    
-    @Override
-    public int insert(Resource resource) {
-        String sql = "INSERT INTO resource(resName, resPath, resType, isCheck) VALUES(?,?,?,?)";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, resource.getResName());
-            preparedStatement.setString(2, resource.getResPath());
-            preparedStatement.setString(3, resource.getResType());
-            preparedStatement.setString(4, resource.getIsCheck());
-            return preparedStatement.executeUpdate();
-        } catch (Exception e) {
-            logger.error(e.toString());
-            return 0;
-        } finally {
-            dataBaseConnection.free(null, preparedStatement, null);
-        }
-    }
+	@Override
+	protected void finalize() {
+		dataBaseConnection.free(connection, preparedStatement, resultSet);
+	}
 
-    @Override
-    public int deleteById(int id){
-        String sql = "delete from resource where id = '"+id+"'";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            return preparedStatement.executeUpdate(sql);
-        } catch (Exception e) {
-            logger.error(e.toString());
-            return 0;
-        } finally {
-            dataBaseConnection.free(null, preparedStatement, null);
-        }
-    }
-    
-    @Override
-    public int getCount(String sql){
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            resultSet = preparedStatement.executeQuery(sql);
-            while (resultSet.next()) {
-                return resultSet.getInt("count1");
-            }
-        } catch (Exception e) {
-            logger.error(e.toString());
-            return 0;
-        } finally {
-            dataBaseConnection.free(null, preparedStatement, null);
-        }
-        return 0;
-    }
+	@Override
+	public List<Resource> selectAll() {
+		List<Resource> resources = new ArrayList<Resource>();
+		Resource resource;
+		String sql = "select id, resName, resTime, resPath, resType, isCheck from resource";
 
-    @Override
-    public List<Resource> showResource(String type) {
-        List<Resource> resources = new ArrayList<>();
-        Resource resource;
-        String sql = "select * from resource where resType like ? and isCheck like ?";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, type);
-            preparedStatement.setString(2, "1");
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                resource = new Resource();
-                resource.setId(resultSet.getInt("id"));
-                resource.setResName(resultSet.getString("resName"));
-                resource.setResTime(resultSet.getTimestamp("resTime"));
-                resource.setResPath(resultSet.getString("resPath"));
-                resource.setResType(resultSet.getString("resType"));
-                resource.setIsCheck(resultSet.getString("isCheck"));
-                resources.add(resource);
-            }
-            return resources;
-        } catch (Exception e) {
-            logger.error(e.toString());
-            return null;
-        } finally {
-            dataBaseConnection.free(null, preparedStatement, resultSet);
-        }
-    }
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			resultSet = preparedStatement.executeQuery(sql);
+			while (resultSet.next()) {
+				resource = new Resource();
+				resource.setId(resultSet.getInt("id"));
+				resource.setResName(resultSet.getString("resName"));
+				resource.setResTime(resultSet.getTimestamp("resTime"));
+				resource.setResPath(resultSet.getString("resPath"));
+				resource.setResType(resultSet.getString("resType"));
+				resource.setIsCheck(resultSet.getString("isCheck"));
+				resources.add(resource);
+			}
+			return resources;
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return null;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, resultSet);
+		}
+	}
+
+	@Override
+	public List<Resource> selectByType(String type) {
+		List<Resource> resources = new ArrayList<Resource>();
+		Resource resource;
+		String sql = "select * from resource where resType like ?";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, type);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				resource = new Resource();
+				resource.setId(resultSet.getInt("id"));
+				resource.setResName(resultSet.getString("resName"));
+				resource.setResTime(resultSet.getTimestamp("resTime"));
+				resource.setResPath(resultSet.getString("resPath"));
+				resource.setResType(resultSet.getString("resType"));
+				resource.setIsCheck(resultSet.getString("isCheck"));
+				resources.add(resource);
+			}
+			return resources;
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return null;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, resultSet);
+		}
+	}
+
+	@Override
+	public List<Resource> selectByDate(Timestamp date1, Timestamp date2, String resType) {
+		List<Resource> resources = new ArrayList<Resource>();
+		Resource resource;
+		String sql = "select id,resName,resTime,resPath,resType,isCheck from resource where resType='" + resType
+				+ "' and resTime between date1 and date2";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			resultSet = preparedStatement.executeQuery(sql);
+			while (resultSet.next()) {
+				resource = new Resource();
+				resource.setId(resultSet.getInt("id"));
+				resource.setResName(resultSet.getString("resName"));
+				resource.setResTime(resultSet.getTimestamp("resTime"));
+				resource.setResPath(resultSet.getString("resPath"));
+				resource.setResType(resultSet.getString("resType"));
+				resource.setIsCheck(resultSet.getString("isCheck"));
+				resources.add(resource);
+			}
+			return resources;
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return null;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, resultSet);
+		}
+	}
+
+	@Override
+	public List<Resource> selectByFuzzyName(String name, String resType) {
+		List<Resource> resources = new ArrayList<>();
+		Resource resource;
+		String sql = "select * from resource where resType='" + resType + "' and resName like '%" + name + "%'";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				resource = new Resource();
+				resource.setId(resultSet.getInt("id"));
+				resource.setResName(resultSet.getString("resName"));
+				resource.setResTime(resultSet.getTimestamp("resTime"));
+				resource.setResPath(resultSet.getString("resPath"));
+				resource.setResType(resultSet.getString("resType"));
+				resource.setIsCheck(resultSet.getString("isCheck"));
+				resources.add(resource);
+			}
+			return resources;
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return null;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, resultSet);
+		}
+	}
+
+	@Override
+	public int update(Resource resource) {
+		String sql = "update resource set resName = ?, resPath = ?, resType = ?, isCheck = ? where id = ?";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, resource.getResName());
+			preparedStatement.setString(2, resource.getResPath());
+			preparedStatement.setString(3, resource.getResType());
+			preparedStatement.setString(4, resource.getIsCheck());
+			preparedStatement.setInt(5, resource.getId());
+			return preparedStatement.executeUpdate();
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return 0;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, null);
+		}
+	}
+
+	@Override
+	public int updateCheck(Resource resource) {
+		String sql = "update resource set isCheck = ? where id = ?";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, resource.getIsCheck());
+			preparedStatement.setInt(2, resource.getId());
+			return preparedStatement.executeUpdate();
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return 0;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, null);
+		}
+	}
+
+	@Override
+	public int insert(Resource resource) {
+		String sql = "INSERT INTO resource(resName, resPath, resType, isCheck) VALUES(?,?,?,?)";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, resource.getResName());
+			preparedStatement.setString(2, resource.getResPath());
+			preparedStatement.setString(3, resource.getResType());
+			preparedStatement.setString(4, resource.getIsCheck());
+			return preparedStatement.executeUpdate();
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return 0;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, null);
+		}
+	}
+
+	@Override
+	public int deleteById(int id) {
+		String sql = "delete from resource where id = '" + id + "'";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			return preparedStatement.executeUpdate(sql);
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return 0;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, null);
+		}
+	}
+
+	@Override
+	public int getCount(String sql) {
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			resultSet = preparedStatement.executeQuery(sql);
+			while (resultSet.next()) {
+				return resultSet.getInt("count1");
+			}
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return 0;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, null);
+		}
+		return 0;
+	}
+
+	@Override
+	public List<Resource> showResource(String type) {
+		List<Resource> resources = new ArrayList<>();
+		Resource resource;
+		String sql = "select * from resource where resType like ? and isCheck like ?";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, type);
+			preparedStatement.setString(2, "1");
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				resource = new Resource();
+				resource.setId(resultSet.getInt("id"));
+				resource.setResName(resultSet.getString("resName"));
+				resource.setResTime(resultSet.getTimestamp("resTime"));
+				resource.setResPath(resultSet.getString("resPath"));
+				resource.setResType(resultSet.getString("resType"));
+				resource.setIsCheck(resultSet.getString("isCheck"));
+				resources.add(resource);
+			}
+			return resources;
+		} catch (Exception e) {
+			logger.error(e.toString());
+			return null;
+		} finally {
+			dataBaseConnection.free(null, preparedStatement, resultSet);
+		}
+	}
 }
---
+++ src/main/java/web/controller/TeacherTeamAction.java
@@ -0,0 +1,223 @@
+package web.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domian.CourseManager;
+import domian.Teacher;
+import domian.TrainingPlan;
+import service.Impl.TeacherTeamService;
+
+
+@WebServlet("/teacherTeam")
+public class TeacherTeamAction extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private TeacherTeamService teacherService = new TeacherTeamService();
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=utf-8");
+		String action =req.getParameter("type");
+		switch (action) {
+
+		case "courseManager": {
+			getCourseManagerMessage(req, resp,0);
+			break;
+		}
+		case "courseManager-edit": {
+			getCourseManagerMessage(req, resp,1);
+			break;
+		}
+		case "courseManager-edits": {
+			getCourseManagerMessage(req, resp,2);
+			break;
+		}
+		case "teacherDetail": {
+			getTeacherDetail(req,resp);
+			break;
+		}
+		case "teacherTeam": {
+			getTeacherTeam(req, resp,0);
+			break;
+		}
+		case "teacherTeam-edit": {
+			getTeacherTeam(req, resp,1);
+			break;
+		}
+		case "teacherTeam-edits": {
+			getTeacherTeam(req, resp,2);
+			break;
+		}
+		case "lecturer": {
+			getTeacherLecturer(req, resp,0);
+			break;
+		}
+		case "lecturer-edit": {
+			getTeacherLecturer(req, resp,1);
+			break;
+		}
+		case "lecturer-edits": {
+			getTeacherLecturer(req, resp,2);
+			break;
+		}
+		case "trainingPlan": {
+			getTrainingPlan(req, resp,0);
+			break;
+		}
+		case "trainingPlan-edit": {
+			getTrainingPlan(req, resp,1);
+			break;
+		}
+		case "trainingPlan-edits": {
+			getTrainingPlan(req, resp,2);
+			break;
+		}
+		default: {
+			break;
+		}
+		}
+	}
+
+
+
+	private void getTeacherDetail(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+			int teacherID = Integer.parseInt(req.getParameter("id"));
+			Teacher teacher = teacherService.getTeacherDetailById(teacherID);
+			if(teacher!=null) {
+				HttpSession session = req.getSession();
+				session.setAttribute("teacher", teacher);
+				try {
+					req.getRequestDispatcher("/html/teacherTeamDetail-5.jsp").forward(req, resp);
+				} catch (ServletException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+	}
+
+
+
+	private void getTeacherLecturer(HttpServletRequest req, HttpServletResponse resp,int mode) {
+		// TODO Auto-generated method stub
+		List<Teacher> allTeacher = teacherService.getTeacherList();
+		List<Teacher> LecturerList = new ArrayList<Teacher>();
+		List<Teacher> NotLecturerList =new ArrayList<Teacher>();
+		if (allTeacher != null) {
+			for(int i=0;i<allTeacher.size();i++)
+				if(allTeacher.get(i).getIsLecturer()==1)
+					LecturerList.add(allTeacher.get(i));
+				else
+					NotLecturerList.add(allTeacher.get(i));
+			HttpSession session = req.getSession();
+			session.setAttribute("LecturerList", LecturerList);
+			session.setAttribute("NotLecturerList", NotLecturerList);
+			try {
+				if(mode==0)
+					req.getRequestDispatcher("/html/teacherTeamDetail-3.jsp").forward(req, resp);
+				if(mode==1)
+					req.getRequestDispatcher("/admin/teacherTeam/teacherLecturerList.jsp").forward(req, resp);
+				if(mode==2)
+					req.getRequestDispatcher("/admin/teacherTeam/teacherLecturerEdit.jsp").forward(req,resp);
+			} catch (ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void getTeacherTeam(HttpServletRequest req, HttpServletResponse resp,int mode) {
+		// TODO Auto-generated method stub
+		List<Teacher> teacherList = teacherService.getTeacherList();
+		if (teacherList != null) {
+			HttpSession session = req.getSession();
+			session.setAttribute("teacherTeam", teacherList);
+			try {
+				if(mode==0)
+					req.getRequestDispatcher("/html/teacherTeamDetail-2.jsp").forward(req, resp);
+				if(mode==1)
+					req.getRequestDispatcher("/admin/teacherTeam/teacherTeamList.jsp").forward(req, resp);
+				if(mode==2) {
+					int teacherId = Integer.parseInt(req.getParameter("id"));
+					for(int i=0;i<teacherList.size();i++) {
+						if(teacherId==teacherList.get(i).getId()) {
+							session.setAttribute("teacherTeam", teacherList.get(i));
+							break;
+						}
+					}
+					req.getRequestDispatcher("/admin/teacherTeam/teacherTeamEdit.jsp").forward(req,resp);
+				}
+			} catch (ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void getCourseManagerMessage(HttpServletRequest req, HttpServletResponse resp,int mode) {
+		CourseManager courseManager = teacherService.getCourseManagerMessage();
+		if (courseManager != null) {
+			HttpSession session = req.getSession();
+			session.setAttribute("courseManager", courseManager);
+			try {
+				if(mode==0)
+					req.getRequestDispatcher("/html/teacherTeamDetail-1.jsp").forward(req, resp);
+				if(mode==1)
+					req.getRequestDispatcher("/admin/teacherTeam/courseManagerList.jsp").forward(req,resp);
+				if(mode==2)
+					req.getRequestDispatcher("/admin/teacherTeam/courseManagerEdit.jsp").forward(req,resp);
+			} catch (ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void getTrainingPlan(HttpServletRequest req, HttpServletResponse resp,int mode) {
+		TrainingPlan trainingPlan = teacherService.getTrainingPlan();
+		if (trainingPlan != null) {
+			HttpSession session = req.getSession();
+			session.setAttribute("trainingPlan", trainingPlan);
+			try {
+				if(mode==0)
+					req.getRequestDispatcher("/html/teacherTeamDetail-4.jsp").forward(req, resp);
+				if(mode==1)
+					req.getRequestDispatcher("/admin/teacherTeam/trainingPlanList.jsp").forward(req, resp);
+				if(mode==2)
+					req.getRequestDispatcher("/admin/teacherTeam/trainingPlanEdit.jsp").forward(req,resp);
+			} catch (ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
